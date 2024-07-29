948318eed6029e3ebbc633536cf5fab2853e32eb
Arnold Ramos Calderon <aramosc.16@hotmail.com>
2024-07-29 12:51:28 -0500

    sidebar 20%

--- src/app/geovisor/components/sidebar/sidebar.component.ts
+++ src/app/geovisor/components/sidebar/sidebar.component.ts
@@ -1,14 +1,19 @@
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgClass} from '@angular/common';
 import {Component} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 
 @Component({
 	selector: 'app-sidebar',
 	standalone: true,
-	imports: [CommonModule, MatIconModule],
+	imports: [CommonModule, MatIconModule, NgClass],
 	templateUrl: './sidebar.component.html',
 	styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
 	public subMenu = 'capas';
+	public toogleMenu = true;
+
+	clickToogleMenu(): void {
+		this.toogleMenu = !this.toogleMenu;
+	}
 }
