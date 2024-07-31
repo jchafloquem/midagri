a1a1cf4b7d82a4be46e8fc304abdbdfb94ccbf6f
Arnold Ramos Calderon <aramosc.16@hotmail.com>
2024-07-31 13:50:12 -0500

    sidebar 25%

--- src/app/geovisor/components/sidebar/sidebar.component.ts
+++ src/app/geovisor/components/sidebar/sidebar.component.ts
@@ -1,19 +1,27 @@
 import {CommonModule, NgClass} from '@angular/common';
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {GeovisorSharedService} from '../../services/geovisor-shared.service';
 
 @Component({
 	selector: 'app-sidebar',
 	standalone: true,
-	imports: [CommonModule, MatIconModule, NgClass],
+	imports: [CommonModule, MatIconModule, NgClass, MatButtonModule],
 	templateUrl: './sidebar.component.html',
 	styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
 	public subMenu = 'capas';
-	public toogleMenu = true;
+	public toogleMenu = false;
+	public geovisorSharedService = inject(GeovisorSharedService);
 
 	clickToogleMenu(): void {
 		this.toogleMenu = !this.toogleMenu;
+	}
+
+	toggleLayer(title: string): void {
+		const currentVisibility = this.geovisorSharedService.getLayerVisibility(title);
+		this.geovisorSharedService.toggleLayerVisibility2(title, !currentVisibility);
 	}
 }
